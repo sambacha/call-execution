20716f228d09354c941debfcb2534ed028b51333
sam bacha <sam@manifoldfinance.com>
2021-09-15 01:54:04 -0700

    chore(lint): format
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

--- contracts/CallPathCallExecutionTree.sol
+++ contracts/CallPathCallExecutionTree.sol
@@ -1,7 +1,6 @@
 // Copyright 2020 ConsenSys Software Inc
 /// SPDX-License-Identifier: Apache-2.0
 
-
 pragma solidity >=0.8.0;
 
 /// @title CallPathCallExecutionTree
@@ -9,16 +8,25 @@ pragma solidity >=0.8.0;
 import "./BytesUtil.sol";
 
 contract CallPathCallExecutionTree is BytesUtil {
+    uint256 private constant NUM_FUNCS_CALLED_SIZE = 1;
+    uint256 private constant OFFSET_SIZE = 4;
+    uint256 private constant BLOCKCHAIN_ID_SIZE = 32;
+    uint256 private constant ADDRESS_SIZE = 20;
+    uint256 private constant DATA_LEN_SIZE_SIZE = 2;
 
-    uint256 constant private NUM_FUNCS_CALLED_SIZE = 1;
-    uint256 constant private OFFSET_SIZE = 4;
-    uint256 constant private BLOCKCHAIN_ID_SIZE = 32;
-    uint256 constant private ADDRESS_SIZE = 20;
-    uint256 constant private DATA_LEN_SIZE_SIZE = 2;
-
-    function extractTargetFromCallGraph(bytes memory _callGraph, uint256[] memory _callPath, bool getFunction) internal pure
-        returns (uint256 targetBlockchainId, address targetContract, bytes memory functionCall) {
-
+    function extractTargetFromCallGraph(
+        bytes memory _callGraph,
+        uint256[] memory _callPath,
+        bool getFunction
+    )
+        internal
+        pure
+        returns (
+            uint256 targetBlockchainId,
+            address targetContract,
+            bytes memory functionCall
+        )
+    {
         uint256 index = 0;
 
         // Go down the call path to the target function
@@ -26,19 +34,24 @@ contract CallPathCallExecutionTree is BytesUtil {
             uint256 offset = 0;
             uint8 numFuncsCalled = BytesUtil.bytesToUint8(_callGraph, index);
             if (numFuncsCalled == 0) {
-                require(i == _callPath.length - 1, "Reached leaf function but there is more call path.");
+                require(
+                    i == _callPath.length - 1,
+                    "Reached leaf function but there is more call path."
+                );
             } else {
                 // Jump to the location of the offset of the function
                 uint256 functionCalled = _callPath[i];
-                offset = BytesUtil.bytesToUint32(_callGraph,
-                    index + functionCalled * OFFSET_SIZE + NUM_FUNCS_CALLED_SIZE);
+                offset = BytesUtil.bytesToUint32(
+                    _callGraph,
+                    index + functionCalled * OFFSET_SIZE + NUM_FUNCS_CALLED_SIZE
+                );
             }
             // Jump to the function
             index = index + offset;
         }
 
         // Jump over the leaf function indicator / numFuncsCalled = 0 indicator
-        if (_callPath[_callPath.length-1] != 0) {
+        if (_callPath[_callPath.length - 1] != 0) {
             uint8 numFuncsCalled = BytesUtil.bytesToUint8(_callGraph, index);
             require(numFuncsCalled == 0, "Expected leaf function.");
             index += NUM_FUNCS_CALLED_SIZE;
@@ -51,14 +64,21 @@ contract CallPathCallExecutionTree is BytesUtil {
             index += ADDRESS_SIZE;
             uint16 functionDataLen = BytesUtil.bytesToUint16(_callGraph, index);
             index += DATA_LEN_SIZE_SIZE;
-            functionCall = BytesUtil.sliceAsm(_callGraph, index, functionDataLen);
-        }
-        else {
+            functionCall = BytesUtil.sliceAsm(
+                _callGraph,
+                index,
+                functionDataLen
+            );
+        } else {
             functionCall = "";
         }
     }
 
-    function determineParentCallPath(uint256[] memory _callPath) internal pure returns (uint256[] memory) {
+    function determineParentCallPath(uint256[] memory _callPath)
+        internal
+        pure
+        returns (uint256[] memory)
+    {
         uint256[] memory callPathOfParent;
         uint256 callPathLen = _callPath.length;
 
@@ -71,8 +91,7 @@ contract CallPathCallExecutionTree is BytesUtil {
                 callPathOfParent[i] = _callPath[i];
             }
             callPathOfParent[callPathLen - 1] = 0;
-        }
-        else {
+        } else {
             callPathOfParent = new uint256[](callPathLen - 1);
             for (uint256 i = 0; i < callPathLen - 2; i++) {
                 callPathOfParent[i] = _callPath[i];
--- contracts/Multigas.sol
+++ contracts/Multigas.sol
@@ -1,26 +1,29 @@
 pragma solidity ^0.8.6;
 pragma experimental ABIEncoderV2;
 
-
 contract MultiCall {
-    
     struct Call {
         address to;
         bytes data;
     }
-    
-   function multicall(Call[] memory calls) public returns (bytes[] memory results) {
+
+    function multicall(Call[] memory calls)
+        public
+        returns (bytes[] memory results)
+    {
         results = new bytes[](calls.length);
-        for (uint i = 0; i < calls.length; i++) {
+        for (uint256 i = 0; i < calls.length; i++) {
             (, results[i]) = calls[i].to.call(calls[i].data);
         }
     }
-    
-    
+
     // @note calls.length == 0
-    function multicallWithGasLimitation(Call[] memory calls, uint256 gasBuffer) public returns (bytes[] memory results, uint256 lastSuccessIndex) {
+    function multicallWithGasLimitation(Call[] memory calls, uint256 gasBuffer)
+        public
+        returns (bytes[] memory results, uint256 lastSuccessIndex)
+    {
         results = new bytes[](calls.length);
-        for (uint i = 0; i < calls.length; i++) {
+        for (uint256 i = 0; i < calls.length; i++) {
             (, results[i]) = calls[i].to.call(calls[i].data);
             if (gasleft() < gasBuffer) {
                 return (results, i);
@@ -28,21 +31,24 @@ contract MultiCall {
         }
         return (results, calls.length - 1);
     }
-    
-   function multicallWithGas(Call[] memory calls) public returns (bytes[] memory results, uint256[] memory gasUsed) {
+
+    function multicallWithGas(Call[] memory calls)
+        public
+        returns (bytes[] memory results, uint256[] memory gasUsed)
+    {
         results = new bytes[](calls.length);
         gasUsed = new uint256[](calls.length);
-        for (uint i = 0; i < calls.length; i++) {
+        for (uint256 i = 0; i < calls.length; i++) {
             uint256 initialGas = gasleft();
             (, results[i]) = calls[i].to.call(calls[i].data);
             gasUsed[i] = initialGas - gasleft();
         }
     }
-    
+
     function gaslimit() external view returns (uint256) {
         return block.gaslimit;
     }
-    
+
     function gasLeft() external view returns (uint256) {
         return gasleft();
     }
